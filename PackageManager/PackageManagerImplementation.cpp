1d8ffae5412465031050896cbbf0e941387d71ce
Suresh Kumar <suresh_kumar3@comcast.com>
2025-04-29 19:47:22 -0700

    Fix fpr rateLimit Getting Applied to Subsequent Downloads

--- PackageManager/PackageManagerImplementation.cpp
+++ PackageManager/PackageManagerImplementation.cpp
@@ -565,7 +565,8 @@ namespace Plugin {
                         LOGTRACE("waitTime=%d retry %d/%d", waitTime, i, di->GetRetries());
                         std::this_thread::sleep_for(std::chrono::seconds(waitTime));
                     }
-                    LOGTRACE("Downloading id=%s url=%s file=%s ", di->GetId().c_str(), di->GetUrl().c_str(), di->GetFileLocator().c_str());
+                    LOGTRACE("Downloading id=%s url=%s file=%s rateLimit=%d",
+                        di->GetId().c_str(), di->GetUrl().c_str(), di->GetFileLocator().c_str(), di->GetRateLimit());
                     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
                     status = mHttpClient->downloadFile(di->GetUrl(), di->GetFileLocator(), di->GetRateLimit());
                     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
