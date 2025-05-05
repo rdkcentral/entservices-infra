ded0ac75a0fca08ab766e3a8d25883deda9a7a9f
veekshagokul <vgokulms@synamedia.com>
2025-05-05 16:30:43 +0530

    Update StorageManagerImplementation.cpp

--- StorageManager/StorageManagerImplementation.h
+++ StorageManager/StorageManagerImplementation.h
@@ -48,21 +48,21 @@ namespace Plugin {
         };
 
     public:
-        typedef struct _StorageAppInfo
-        {
-            std::string path;    /* Path to the application's storage */
-            int32_t uid;         /* UID of the user who owns the storage */
-            int32_t gid;         /* GID of the group who owns the storage */
-            uint32_t quotaKB;    /* Quota size in kilobytes for the storage */
-            uint32_t usedKB;     /* Used space in kilobytes for the storage */
-            std::mutex storageLock; /* Mutex for thread safety */
-        } StorageAppInfo;
+        // typedef struct _StorageAppInfo
+        // {
+        //     std::string path;    /* Path to the application's storage */
+        //     int32_t uid;         /* UID of the user who owns the storage */
+        //     int32_t gid;         /* GID of the group who owns the storage */
+        //     uint32_t quotaKB;    /* Quota size in kilobytes for the storage */
+        //     uint32_t usedKB;     /* Used space in kilobytes for the storage */
+        //     std::mutex storageLock; /* Mutex for thread safety */
+        // } StorageAppInfo;
 
-        typedef struct _StorageSize
-        {
-            uint64_t blockSize = 0;
-            uint64_t usedBytes = 0;
-        } StorageSize;
+        // typedef struct _StorageSize
+        // {
+        //     uint64_t blockSize = 0;
+        //     uint64_t usedBytes = 0;
+        // } StorageSize;
 
         StorageManagerImplementation();
         ~StorageManagerImplementation() override;
@@ -75,29 +75,29 @@ namespace Plugin {
         INTERFACE_ENTRY(Exchange::IConfiguration)
         END_INTERFACE_MAP
 
-        Core::hresult CreateStorage(const string& appId, const uint32_t& size, string& path, string& errorReason) override;
-        Core::hresult GetStorage(const string& appId, const int32_t& userId, const int32_t& groupId, string& path, uint32_t& size, uint32_t& used) override;
-        Core::hresult DeleteStorage(const string& appId, string& errorReason) override;
-        Core::hresult Clear(const string& appId, string& errorReason) override;
-        Core::hresult ClearAll(const string& exemptionAppIds, string& errorReason) override;
+        virtual Core::hresult CreateStorage(const string& appId, const uint32_t& size, string& path, string& errorReason) override;
+        virtual Core::hresult GetStorage(const string& appId, const int32_t& userId, const int32_t& groupId, string& path, uint32_t& size, uint32_t& used) override;
+        virtual Core::hresult DeleteStorage(const string& appId, string& errorReason) override;
+        virtual Core::hresult Clear(const string& appId, string& errorReason) override;
+        virtual Core::hresult ClearAll(const string& exemptionAppIds, string& errorReason) override;
 
         // IConfiguration methods
         uint32_t Configure(PluginHost::IShell* service) override;
 
-    private:
-        bool createAppStorageInfoByAppID(const std::string& appId, StorageAppInfo &storageInfo);
-        bool retrieveAppStorageInfoByAppID(const string &appId, StorageAppInfo &storageInfo);
-        bool removeAppStorageInfoByAppID(const string &appId);
-        bool hasEnoughStorageFreeSpace(const std::string& baseDir, uint32_t requiredSpaceKB);
-        uint64_t getDirectorySizeInBytes(const std::string &path);
-        static int getSize(const char *path, const struct stat *statPtr, int currentFlag, struct FTW *internalFtwUsage);
-        Core::hresult deleteDirectoryEntries(const string& appId, string& errorReason);
-        bool lockAppStorageInfo(const std::string& appId, std::unique_lock<std::mutex>& appLock);
+    // private:
+    //     bool createAppStorageInfoByAppID(const std::string& appId, StorageAppInfo &storageInfo);
+    //     bool retrieveAppStorageInfoByAppID(const string &appId, StorageAppInfo &storageInfo);
+    //     bool removeAppStorageInfoByAppID(const string &appId);
+    //     bool hasEnoughStorageFreeSpace(const std::string& baseDir, uint32_t requiredSpaceKB);
+    //     uint64_t getDirectorySizeInBytes(const std::string &path);
+    //     static int getSize(const char *path, const struct stat *statPtr, int currentFlag, struct FTW *internalFtwUsage);
+    //     Core::hresult deleteDirectoryEntries(const string& appId, string& errorReason);
+    //     bool lockAppStorageInfo(const std::string& appId, std::unique_lock<std::mutex>& appLock);
 
     private:
         mutable std::mutex mStorageManagerImplLock;
-        static std::mutex mStorageSizeLock;
-        std::map<std::string, StorageAppInfo> mStorageAppInfo;  /* Map storing app storage info for each appId */
+        //static std::mutex mStorageSizeLock;
+        //std::map<std::string, StorageAppInfo> mStorageAppInfo;  /* Map storing app storage info for each appId */
         Config _config;
         PluginHost::IShell* mCurrentservice;
         std::string mBaseStoragePath;                           /* Base path for the app storage */
