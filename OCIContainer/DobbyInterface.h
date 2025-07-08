f9c94f90cec2e395a39c9afcb4f232d68ed9a5f2
Sankalp <sankalp_maneshwar@comcast.com>
2025-07-08 13:41:06 +0530

    RDK-55559 : Add L1 & L2 tests for OCIContainer plugin Reason for change: Adding L2 test cases for latest COM-RPC supported OCIContainer plugin. Test Procedure: Run L2test to verify Risks: Medium
    Signed-off-by: Sankalp <sankalp_maneshwar@comcast.com>

--- OCIContainer/DobbyInterface.h
+++ OCIContainer/DobbyInterface.h
@@ -24,7 +24,11 @@
 #include <interfaces/IOCIContainer.h>
 #include <vector>
 #include <map>
+#ifndef RDK_SERVICE_L2_TEST
 #include <i_omi_proxy.hpp>
+#else
+#include "Omi.h"
+#endif
 #include "IEventHandler.h"
 
 namespace WPEFramework
