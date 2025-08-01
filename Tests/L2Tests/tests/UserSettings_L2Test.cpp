ac645b2846a73b6e13b8c479f3271ac910797a36
Prathyushakothuru <142974139+Prathyushakothuru@users.noreply.github.com>
2025-08-01 17:22:47 +0530

    Update UserSettings_L2Test.cpp

--- Tests/L2Tests/tests/UserSettings_L2Test.cpp
+++ Tests/L2Tests/tests/UserSettings_L2Test.cpp
@@ -26,7 +26,9 @@
 #include <fstream>
 #include <interfaces/IStore2.h>
 #include <interfaces/IUserSettings.h>
-#include "WaitGroup.h"
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
 
 #define TEST_LOG(x, ...) fprintf(stderr, "\033[1;32m[%s:%d](%s)<PID:%d><TID:%d>" x "\n\033[0m", __FILE__, __LINE__, __FUNCTION__, getpid(), gettid(), ##__VA_ARGS__); fflush(stderr);
 
@@ -39,6 +41,44 @@ using namespace WPEFramework;
 using testing::StrictMock;
 using ::WPEFramework::Exchange::IStore2;
 using ::WPEFramework::Exchange::IUserSettings;
+
+class WaitGroup {
+public:
+    WaitGroup()
+        : _count(0)
+    {
+    }
+
+    void Add(int count = 1)
+    {
+        _count += count;
+
+        if (_count <= 0)
+            _cv.notify_all();
+    }
+
+    void Done() { Add(-1); }
+
+    void Wait()
+    {
+        if (_count <= 0)
+            return;
+
+        std::unique_lock<std::mutex> _lock{ _m };
+        _cv.wait(_lock, [&]() {
+            return _count == 0;
+        });
+    }
+
+    // avoid copies of this class
+    WaitGroup(const WaitGroup&) = delete;
+    WaitGroup operator=(const WaitGroup&) = delete;
+
+private:
+    std::atomic<int> _count;
+    std::mutex _m;
+    std::condition_variable _cv;
+};
 
 typedef enum : uint32_t {
     UserSettings_onAudioDescriptionChanged = 0x00000001,
