eba08035ed9828b7dfefae92cae17ae42283d205
kprathyusha <kprathyusha@synamedia.com>
2025-08-06 16:06:51 +0530

    resolving review comments

--- Tests/L2Tests/tests/UserSettings_L2Test.cpp
+++ Tests/L2Tests/tests/UserSettings_L2Test.cpp
@@ -26,7 +26,6 @@
 #include <fstream>
 #include <interfaces/IStore2.h>
 #include <interfaces/IUserSettings.h>
-#include <atomic>
 #include <condition_variable>
 #include <mutex>
 #include <time.h>
@@ -43,44 +42,6 @@ using namespace WPEFramework;
 using testing::StrictMock;
 using ::WPEFramework::Exchange::IStore2;
 using ::WPEFramework::Exchange::IUserSettings;
-
-// class WaitGroup {
-// public:
-//     WaitGroup()
-//         : _count(0)
-//     {
-//     }
-
-//     void Add(int count = 1)
-//     {
-//         _count += count;
-
-//         if (_count <= 0)
-//             _cv.notify_all();
-//     }
-
-//     void Done() { Add(-1); }
-
-//     void Wait()
-//     {
-//         if (_count <= 0)
-//             return;
-
-//         std::unique_lock<std::mutex> _lock{ _m };
-//         _cv.wait(_lock, [&]() {
-//             return _count == 0;
-//         });
-//     }
-
-//     // avoid copies of this class
-//     WaitGroup(const WaitGroup&) = delete;
-//     WaitGroup operator=(const WaitGroup&) = delete;
-
-// private:
-//     std::atomic<int> _count;
-//     std::mutex _m;
-//     std::condition_variable _cv;
-// };
 
 typedef enum : uint32_t {
     UserSettings_onAudioDescriptionChanged = 0x00000001,
