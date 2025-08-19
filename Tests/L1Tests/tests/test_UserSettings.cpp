8da0b290ec47809d3bdb6f56c7515491878e325b
andrejz2 <113075729+andrejz2@users.noreply.github.com>
2025-08-19 18:03:20 -0400

    Update test_UserSettings.cpp

--- Tests/L1Tests/tests/test_UserSettings.cpp
+++ Tests/L1Tests/tests/test_UserSettings.cpp
@@ -16,6 +16,7 @@
 #include "COMLinkMock.h"
 #include "WrapsMock.h"
 #include "ThunderPortability.h"
+#include "WorkerPoolImplementation.h"
 #define TEST_LOG(x, ...) fprintf(stderr, "\033[1;32m[%s:%d](%s)<PID:%d><TID:%d>" x "\n\033[0m", __FILE__, __LINE__, __FUNCTION__, getpid(), gettid(), ##__VA_ARGS__); fflush(stderr);
 
 using ::testing::NiceMock;
