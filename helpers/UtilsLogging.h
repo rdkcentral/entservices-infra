7248c66face856186e753a91af5595005b214eba
HarshiniRavichandran <166796069+HarshiniRavichandran@users.noreply.github.com>
2025-08-19 12:28:08 +0530

    RDKEMW-5810 : Update UtilsLogging.h

--- helpers/UtilsLogging.h
+++ helpers/UtilsLogging.h
@@ -29,7 +29,7 @@
 
 enum LogLevel {FATAL_LEVEL = 0, ERROR_LEVEL, WARNING_LEVEL, INFO_LEVEL, DEBUG_LEVEL};
 
-static int gDefaultLogLevel __attribute__((unused)) = ERROR_LEVEL;
+static int gDefaultLogLevel = ERROR_LEVEL;
 
 #define LOGDBG(fmt, ...) do { if(gDefaultLogLevel >= DEBUG_LEVEL) { fprintf(stderr, "[%d] DEBUG [%s:%d] %s: " fmt "\n", (int)syscall(SYS_gettid), WPEFramework::Core::FileNameOnly(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__); fflush(stderr); }} while (0)
 #define LOGINFO(fmt, ...) do { if(gDefaultLogLevel >= INFO_LEVEL) { fprintf(stderr, "[%d] INFO [%s:%d] %s: " fmt "\n", (int)syscall(SYS_gettid), WPEFramework::Core::FileNameOnly(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__); fflush(stderr); }} while (0)
