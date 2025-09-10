720465f4537b303d686118cd48049ecbd12b6b1c
madanagopalt <madanagopal123@gmail.com>
2025-09-10 15:26:59 +0530

    RDKEMW-7855: Address copilot review comments

--- LifecycleManager/StateHandler.cpp
+++ LifecycleManager/StateHandler.cpp
@@ -166,10 +166,6 @@ namespace WPEFramework
             Exchange::ILifecycleManager::LifecycleState lifecycleState = request.mTargetState;
             ApplicationContext* context = request.mContext;
 
-            if (context == nullptr) {
-                errorReason = "ApplicationContext is null";
-                return false;
-            }
             if (context == nullptr)
             {
                 errorReason = "ApplicationContext is null";
