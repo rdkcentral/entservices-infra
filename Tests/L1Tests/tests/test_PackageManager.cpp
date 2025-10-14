36dd37a254934a4e712f5029fba0324908019035
IshvarKR <ishvarrr01@gmail.com>
2025-10-14 01:40:00 +0000

    Update in test_PackageManager.cpp

--- Tests/L1Tests/tests/test_PackageManager.cpp
+++ Tests/L1Tests/tests/test_PackageManager.cpp
@@ -38,7 +38,7 @@
 #include "FactoriesImplementation.h"
 
 #define TEST_LOG(x, ...) fprintf(stderr, "\033[1;32m[%s:%d](%s)<PID:%d><TID:%d>" x "\n\033[0m", __FILE__, __LINE__, __FUNCTION__, getpid(), gettid(), ##__VA_ARGS__); fflush(stderr);
-#define TIMEOUT   (1000)
+#define TIMEOUT   (3000)
 
 using ::testing::NiceMock;
 using namespace WPEFramework;
