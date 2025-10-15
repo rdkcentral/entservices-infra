e45670993d6ae7c5b0f490494ef4057a76a5a72e
IshvarKR <ishvarrr01@gmail.com>
2025-10-15 01:11:06 +0000

    RDKEMW-2803: [AI2.0][PackageManager ] Add L1 tests
    Reason for change: Addition of L1 test cases for Package Manager
    Test Procedure: None
    Priority: P2
    Risks: None
    Signed-off-by: IshvarAdithya_KavasseryRamanathan@comcast.com

--- Tests/L1Tests/tests/test_PackageManager.cpp
+++ Tests/L1Tests/tests/test_PackageManager.cpp
@@ -38,7 +38,7 @@
 #include "FactoriesImplementation.h"
 
 #define TEST_LOG(x, ...) fprintf(stderr, "\033[1;32m[%s:%d](%s)<PID:%d><TID:%d>" x "\n\033[0m", __FILE__, __LINE__, __FUNCTION__, getpid(), gettid(), ##__VA_ARGS__); fflush(stderr);
-#define TIMEOUT   (1000)
+#define TIMEOUT   (2000)
 
 #define DEBUG_PRINTF(fmt, ...) \
     std::printf("[DEBUG] %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
