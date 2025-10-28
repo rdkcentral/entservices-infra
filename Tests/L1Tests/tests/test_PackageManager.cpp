6569c3977a63c67c5cc7f9844df0b82545128b9c
IshvarKR <130352532+IshvarKR@users.noreply.github.com>
2025-10-28 06:54:37 +0530

    Update test_PackageManager.cpp

--- Tests/L1Tests/tests/test_PackageManager.cpp
+++ Tests/L1Tests/tests/test_PackageManager.cpp
@@ -2024,7 +2024,7 @@ TEST_F(PackageManagerTest, packageStateusingJsonRpcSuccess) {
 	
     releaseResources();
 }
-#endif
+
 /* Test Case for package state failure using ComRpc
  * 
  * Set up and initialize required COM-RPC resources, configurations, mocks and expectations
@@ -2092,7 +2092,7 @@ TEST_F(PackageManagerTest, packageStateusingComRpcSuccess) {
 	
     releaseResources();
 }
-
+#endif
  /* Test Case for get config for package error due to invalid signature using JsonRpc
  * 
  * Set up and initialize required JSON-RPC resources, configurations, mocks and expectations
