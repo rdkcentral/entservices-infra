92e2039ba0cb2b33ab4c30cf6c068172bd618937
IshvarKR <130352532+IshvarKR@users.noreply.github.com>
2025-10-29 09:19:24 +0530

    Update test_PackageManager.cpp

--- Tests/L1Tests/tests/test_PackageManager.cpp
+++ Tests/L1Tests/tests/test_PackageManager.cpp
@@ -541,7 +541,7 @@ TEST_F(PackageManagerTest, pauseMethodusingJsonRpcSuccess) {
 
     EXPECT_EQ(Core::ERROR_NONE, mJsonRpcHandler.Invoke(connection, _T("download"), _T("{\"url\": \"https://www.examplefile.com/file-download/328\"}"), mJsonRpcResponse));
 
-    waitforJsonRpc(300);
+    waitforJsonRpc(200);
 
     EXPECT_NE(mJsonRpcResponse.find("1001"), std::string::npos);
 
@@ -676,7 +676,7 @@ TEST_F(PackageManagerTest, resumeMethodusingJsonRpcSuccess) {
 
     EXPECT_EQ(Core::ERROR_NONE, mJsonRpcHandler.Invoke(connection, _T("download"), _T("{\"url\": \"https://www.examplefile.com/file-download/328\"}"), mJsonRpcResponse));
 
-    waitforJsonRpc(300);
+    waitforJsonRpc(200);
 
     EXPECT_NE(mJsonRpcResponse.find("1001"), std::string::npos);
 
@@ -816,7 +816,7 @@ TEST_F(PackageManagerTest, cancelMethodusingJsonRpcSuccess) {
 
     EXPECT_EQ(Core::ERROR_NONE, mJsonRpcHandler.Invoke(connection, _T("download"), _T("{\"url\": \"https://www.examplefile.com/file-download/328\"}"), mJsonRpcResponse));
 
-    waitforJsonRpc(300);
+    waitforJsonRpc(200);
 
     EXPECT_NE(mJsonRpcResponse.find("1001"), std::string::npos);
 
