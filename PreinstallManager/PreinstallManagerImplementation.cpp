ee1be6cac7ba024ec3aebdab7d7be2e0ce645d18
sangeetha-ai61 <Sangeetha_Panneerselvam@comcast.com@comcast.com>
2025-11-01 05:50:08 +0000

    RDKEMW-8855-new

--- PreinstallManager/PreinstallManagerImplementation.cpp
+++ PreinstallManager/PreinstallManagerImplementation.cpp
@@ -21,11 +21,7 @@
 
 #include "PreinstallManagerImplementation.h"
 
-#ifdef UNIT_TEST_BUILD
-    #define AI_PREINSTALL_DIRECTORY "/tmp/preinstall" //test directory for unit tests
-#else
-    #define AI_PREINSTALL_DIRECTORY "/opt/preinstall" //temporary directory for preinstaLl packages
-#endif
+#define AI_PREINSTALL_DIRECTORY "/opt/preinstall" //temporary directory for preinstaLl packages
 
 namespace WPEFramework
 {
