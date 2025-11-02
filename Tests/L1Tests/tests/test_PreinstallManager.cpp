a99940ba3e326a2b16d77428d378dd8770440a84
sangeetha-ai61 <Sangeetha_Panneerselvam@comcast.com>
2025-11-02 10:28:40 +0530

    Update test_PreinstallManager.cpp

--- Tests/L1Tests/tests/test_PreinstallManager.cpp
+++ Tests/L1Tests/tests/test_PreinstallManager.cpp
@@ -42,6 +42,8 @@
 #include "WrapsMock.h"
 #include "FactoriesImplementation.h"
 
+#define UNIT_TESTING
+
 #define TEST_LOG(x, ...) fprintf(stderr, "\033[1;32m[%s:%d](%s)<PID:%d><TID:%d>" x "\n\033[0m", __FILE__, __LINE__, __FUNCTION__, getpid(), gettid(), ##__VA_ARGS__); fflush(stderr);
 
 #define TIMEOUT   (50000)
