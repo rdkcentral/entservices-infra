fc5758023b9bbde2ce1eeab4c13ec0ab854be28b
agampa263 <133222558+agampa263@users.noreply.github.com>
2026-01-26 22:04:18 +0530

    Update ResourceManager.cpp

--- ResourceManager/ResourceManager.cpp
+++ ResourceManager/ResourceManager.cpp
@@ -52,28 +52,23 @@ namespace WPEFramework {
         ResourceManager* ResourceManager::_instance = nullptr;
         ResourceManager::ResourceManager()
             : PluginHost::JSONRPC()
-            // Fix for Coverity issue 1146 (UNINIT_CTOR): Initialize all member variables in declaration order
-#if defined(ENABLE_ERM) || defined(ENABLE_L1TEST)
-            , mEssRMgr(nullptr)
-#endif
-            , mDisableBlacklist(true)
-            , mDisableReserveTTS(true)
-            , mCurrentService(nullptr)
         {
             Register<JsonObject, JsonObject>(_T(RESOURCE_MANAGER_METHOD_SET_AV_BLOCKED), &ResourceManager::setAVBlockedWrapper, this);
             Register<JsonObject, JsonObject>(_T(RESOURCE_MANAGER_METHOD_GET_BLOCKED_AV_APPLICATIONS), &ResourceManager::getBlockedAVApplicationsWrapper, this);
             Register<JsonObject, JsonObject>(_T(RESOURCE_MANAGER_METHOD_RESERVE_TTS_RESOURCE), &ResourceManager::reserveTTSResourceWrapper, this);
 	    Register<JsonObject, JsonObject>(_T(RESOURCE_MANAGER_METHOD_RESERVE_TTS_RESOURCE_FOR_APPS), &ResourceManager::reserveTTSResourceWrapperForApps, this);
+			mEssRMgr = nullptr;
 
 #ifdef ENABLE_ERM
             mEssRMgr = EssRMgrCreate();
             std::cout<<"EssRMgrCreate "<<((mEssRMgr != nullptr)?"succeeded":"failed")<<std::endl;
 
             RFC_ParamData_t param;
-            // Fix for Coverity issue 1146 (UNINIT_CTOR): Values now initialized in initialization list
-            // but can still be overridden by RFC settings
-            // mDisableBlacklist = true;
-            // mDisableReserveTTS = true;
+            //Fix for Coverity issue 1146 (UNINIT_CTOR): Values now initialized in initialization list
+            //but can still be overridden by RFC settings
+            mDisableBlacklist = true;
+            mDisableReserveTTS = true;
+			mCurrentService = nullptr;
 
             if (true == Utils::getRFCConfig("Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Resourcemanager.Blacklist.Enable", param))
             {
