91fd9191e09f9b9b2c23735b0a645ab1348489a7
naveen-0206 <naveenkumar.t@ltts.com>
2026-02-23 11:21:56 +0530

    RDKEMW-999 : Add COM-RPC support to ResourceManager plugin Reason For Change: Adding plugin fix Test procedure : Test ResourceManager Priority: P1
    Signed-off-by: naveen-0206 <naveenkumar.t@ltts.com>

--- ResourceManager/ResourceManager.h
+++ ResourceManager/ResourceManager.h
@@ -22,6 +22,7 @@
 #include "Module.h"
 #include <interfaces/IResourceManager.h>
 #include <interfaces/json/JResourceManager.h>
+#include <interfaces/json/JsonData_ResourceManager.h>
 
 namespace WPEFramework
 {
@@ -29,6 +30,41 @@ namespace WPEFramework
     {
         class ResourceManager : public PluginHost::IPlugin, public PluginHost::JSONRPC
         {
+            private:
+                class Notification : public RPC::IRemoteConnection::INotification
+                {
+                    private:
+                        Notification() = delete;
+                        Notification(const Notification&) = delete;
+                        Notification& operator=(const Notification&) = delete;
+
+                    public:
+                        explicit Notification(ResourceManager* parent)
+                            : _parent(*parent)
+                        {
+                            ASSERT(parent != nullptr);
+                        }
+                        virtual ~Notification()
+                        {
+                        }
+
+                        BEGIN_INTERFACE_MAP(Notification)
+                            INTERFACE_ENTRY(RPC::IRemoteConnection::INotification)
+                        END_INTERFACE_MAP
+
+                        void Activated(RPC::IRemoteConnection*) override
+                        {
+                        }
+
+                        void Deactivated(RPC::IRemoteConnection *connection) override
+                        {
+                            _parent.Deactivated(connection);
+                        }
+
+                    private:
+                        ResourceManager& _parent;
+                };
+
             public:
                 ResourceManager(const ResourceManager&) = delete;
                 ResourceManager& operator=(const ResourceManager&) = delete;
@@ -43,14 +79,19 @@ namespace WPEFramework
                 END_INTERFACE_MAP
 
                 //  IPlugin methods
+                // ------------------------------------------------------------------------------------------
                 const string Initialize(PluginHost::IShell* service) override;
                 void Deinitialize(PluginHost::IShell* service) override;
                 string Information() const override;
 
             private:
+                void Deactivated(RPC::IRemoteConnection* connection);
+
+            private:
                 PluginHost::IShell* _service{};
                 uint32_t _connectionId{};
                 Exchange::IResourceManager* _resourceManager{};
+                Core::Sink<Notification> _resourceManagerNotification;
         };
     } // namespace Plugin
 } // namespace WPEFramework
